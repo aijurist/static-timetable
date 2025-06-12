4d8dbe9e739335cd6e873bcd15b88d4d4b1ec80b
Shanthosh S <shanthosh811@gmail.com>
2025-06-12 06:05:34 +0530

    chore: added csv

--- timetable/config.py
+++ timetable/config.py
@@ -19,24 +19,24 @@ THEORY_TIME_SLOTS = [
 
 # Lab time slots (12 slots with 2 continuous slots per lab)
 LAB_TIME_SLOTS = [
-    ("8:00", "9:40"),     # Lab 1 (2 slots)
-    ("9:50", "11:30"),    # Lab 2 (2 slots)
-    ("11:50", "13:30"),   # Lab 3 (2 slots)
-    ("13:50", "15:30"),   # Lab 4 (2 slots)
-    ("15:50", "17:30"),   # Lab 5 (2 slots)
-    ("17:50", "19:30")    # Lab 6 (2 slots)
+    ("8:00", "9:40"),     
+    ("9:50", "11:30"),    
+    ("11:50", "13:30"),   
+    ("13:50", "15:30"),   
+    ("15:50", "17:30"),   
+    ("17:50", "19:30")    
 ]
 
 MAX_TEACHER_HOURS = 21
 CLASS_STRENGTH = 70
 LAB_BATCH_SIZE = 35
-LAB_DURATION_SLOTS = 2  # Each lab takes 2 continuous slots
+LAB_DURATION_SLOTS = 2 
 
 # Teacher shift configurations
 SHIFTS = {
-    'MORNING': (time(8, 0), time(15, 0)),    # 8 AM - 3 PM
-    'AFTERNOON': (time(10, 0), time(17, 0)),  # 10 AM - 5 PM
-    'EVENING': (time(12, 0), time(19, 0))     # 12 PM - 7 PM
+    'MORNING': (time(8, 0), time(15, 0)),    
+    'AFTERNOON': (time(10, 0), time(17, 0)), 
+    'EVENING': (time(12, 0), time(19, 0))    
 }
 
 SHIFT_PATTERNS = [
@@ -52,4 +52,101 @@ SHIFT_PATTERNS = [
     {'MORNING': 1, 'AFTERNOON': 2, 'EVENING': 2},
     {'MORNING': 1, 'EVENING': 2, 'AFTERNOON': 2},
     {'AFTERNOON': 1, 'EVENING': 2, 'MORNING': 2}
-]# Each lab takes 2 continuous slots
+]
+
+DEPARTMENT_DATA = {
+    "CSE-CS": {
+        "2": 2,
+        "3": 1
+    },
+    "CSE": {
+        "2": 10,
+        "3": 6,
+        "4": 5
+    },
+    "CSBS": {
+        "2": 2,
+        "3": 2,
+        "4": 2
+    },
+    "CSD": {
+        "2": 1,
+        "3": 1,
+        "4": 1
+    },
+    "IT": {
+        "2": 5,
+        "3": 4,
+        "4": 3
+    },
+    "AIML": {
+        "2": 4,
+        "3": 3,
+        "4": 3
+    },
+    "AIDS": {
+        "2": 5,
+        "3": 3,
+        "4": 1
+    },
+    "ECE": {
+        "2": 6,
+        "3": 4,
+        "4": 4
+    },
+    "EEE": {
+        "2": 2,
+        "3": 2,
+        "4": 2
+    },
+    "AERO": {
+        "2": 1,
+        "3": 1,
+        "4": 1
+    },
+    "AUTO": {
+        "2": 1,
+        "3": 1,
+        "4": 1
+    },
+    "MCT": {
+        "2": 1,
+        "3": 1,
+        "4": 1
+    },
+    "MECH": {
+        "2": 2,
+        "3": 2,
+        "4": 2
+    },
+    "BT": {
+        "2": 3,
+        "3": 3,
+        "4": 3
+    },
+    "BME": {
+        "2": 2,
+        "3": 2,
+        "4": 2
+    },
+    "R&A": {
+        "2": 1,
+        "3": 1,
+        "4": 1
+    },
+    "FT": {
+        "2": 1,
+        "3": 1,
+        "4": 1
+    },
+    "CIVIL": {
+        "2": 1,
+        "3": 1,
+        "4": 1
+    },
+    "CHEM": {
+        "2": 1,
+        "3": 1,
+        "4": 1
+    }
+}
